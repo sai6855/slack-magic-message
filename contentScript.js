3689620374c658a3dc2720d1024c99cbd761bba7
sai6855 <60743144+sai6855@users.noreply.github.com>
2024-04-29 11:30:20 +0530

    Update icon color based on theme

--- contentScript.js
+++ contentScript.js
@@ -16,7 +16,13 @@ function generateIcon() {
     "m19 9 1.25-2.75L23 5l-2.75-1.25L19 1l-1.25 2.75L15 5l2.75 1.25zm-7.5.5L9 4 6.5 9.5 1 12l5.5 2.5L9 20l2.5-5.5L17 12zM19 15l-1.25 2.75L15 19l2.75 1.25L19 23l1.25-2.75L23 19l-2.75-1.25z"
   );
 
-  path.setAttribute("fill", "white");
+  if (document.querySelector(".sk-client-theme--dark")) {
+    // dark mode
+    path.setAttribute("fill", "white");
+  } else {
+    // light mode
+    path.setAttribute("fill", "black");
+  }
 
   svg.appendChild(path);
 
